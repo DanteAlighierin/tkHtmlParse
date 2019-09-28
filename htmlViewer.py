b5d4ecd895a1e7bb62335433bc9b7a349efb372f
dante <zhivchikovv2@gmail.com>
2019-09-28 21:36:27 +0300

    add main file

---
+++ htmlViewer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+from tkinter import *
+import urllib.request
+def go():
+    text.delete(1.0, END)
+    with urllib.request.urlopen(entry.get()) as response:
+        received_html = response.read()
+    text.insert(1.0, received_html)
+
+browser_window = Tk()
+browser_window.title("Dante's browser")
+label = Label(browser_window, text= 'Enter URL:')
+entry = Entry(browser_window)
+entry.insert(END, "https://www.linuxmint.com/start/tessa/")
+button = Button(browser_window, text='Go', command = go)
+text = Text(browser_window)
+label.pack(side=TOP)
+entry.pack(side=TOP)
+button.pack(side=TOP)
+text.pack(side= TOP)
+browser_window.mainloop()
+
+
+
